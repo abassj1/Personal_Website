656912c633d87f62b1dbf56c438246ef93536c61
abassj1 <abass-jam@hotmail.com>
2023-01-11 15:58:23 +0000

    Fixed logo problem by simply adding the object with src attribute to each post

--- src/partials/PostsList.jsx
+++ src/partials/PostsList.jsx
@@ -6,12 +6,11 @@ const PostsList = () => {
 const [title, setTitle] = useState('');
 const [description, setDescription] = useState('');
 const [buttonLink, setButtonLink] = useState('');
-const [imgObject, setImgObject] = useState('');
 const [postContainer, setPostContainer] = useState([]);
 
 const handleSubmit = (event) => {
     event.preventDefault();
-    const newPost = createPostComponent(title, description, buttonLink, imgObject);
+    const newPost = createPostComponent(title, description, buttonLink);
     setPostContainer([...postContainer, newPost]);
 };
 
@@ -20,29 +19,34 @@ return (
         <div class="container flex flex-col mx-auto max-w-[1000px] h-[100%]">
             <div className="mx-auto mt-16 w-full h-[100%] max-w-[1000px] items-center text-center">
                 <div className="mx-auto lg:mx-4 font-bold text-4xl text-blitblue-500">Posts</div>
-
-                <form onSubmit={handleSubmit}>
-                    <label>
-                        Title:
-                        <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
-                    </label>
-                    <br />
-                    <label>
-                        Description:
-                        <input type="text" value={description} onChange={(event) => setDescription(event.target.value)} />
-                    </label>
-                    <br />
-                    <label>
-                        Button Link:
-                        <input type="text" value={buttonLink} onChange={(event) => setButtonLink(event.target.value)} />
-                    </label>
-                    <br />
-                    <button type="submit">Create Post</button>
-                </form>
-                <div class="post-container mt-8 grid md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-16">
-                    {postContainer.map((post, index) => {
-                    return <div key={index} dangerouslySetInnerHTML={{ __html: post }} />
-                    })}
+                <div class="mx-auto items-center">
+                    <form onSubmit={handleSubmit}>
+                        <label class="ml-12 text-blitblue-500">
+                            Title:
+                            <input class="ml-6 text-black" type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
+                        </label>
+                        <br />
+                        <label class="text-blitblue-500">
+                            Description:
+                            <input class="ml-6 text-black" type="text" value={description} onChange={(event) => setDescription(event.target.value)} />
+                        </label>
+                        <br />
+                        <label class="text-blitblue-500">
+                            Button Link:
+                            <input class="ml-6 text-black" type="text" value={buttonLink} onChange={(event) => setButtonLink(event.target.value)} />
+                        </label>
+                        <br />
+                        <div class="mx-auto mt-2 items-center">                    
+                            <button type="submit" className="viewBtn leading-tight tracking-tight"> Create Post
+                                <span></span>
+                            </button>  
+                        </div>
+                    </form>
+                    <div class="post-container mt-8 grid md:grid-cols-2 lg:grid-cols-3 grid-rows-1 gap-16">
+                        {postContainer.map((post, index) => {
+                        return <div key={index} dangerouslySetInnerHTML={{ __html: post }} />
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
